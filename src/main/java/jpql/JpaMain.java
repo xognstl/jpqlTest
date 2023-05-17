c30ce00a3c59232648646fbc3b96bd64bcf1bfd2
xognstl <xognstl@naver.com>
2023-05-17 23:00:48 +0900

    z

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,46 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result
+                    = em.createQuery("select distinct new jpql.MemberDTO(m.username, m.age) from Member m ", MemberDTO.class).getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getAge());
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+
+
+
+}
